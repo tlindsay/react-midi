cb4a9ad2c3fcfdbd07ef106be9096fe7722d23bc
Patrick Lindsay <patrick.lindsay@dockyard.com>
2020-04-17 12:33:28 -0500

    --wip-- [skip ci]

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import WebMidi from 'webmidi';
 import retry from 'async-retry';
 
@@ -23,18 +22,30 @@ class MidiContext extends Component {
         if (!err) {
           console.info('MIDI enabled');
           this.state.inputs = WebMidi.inputs;
+
+          if (this.props.debug) {
+            this.addDebugListeners();
+          }
         }
       });
     });
   }
 
+  addDebugListeners() {
+    this.state.inputs.forEach((input) => {
+      input.addListener('noteon', 'all', () => console.log('noteon'));
+      input.addListener('pitchbend', 'all', () => console.log('pitchbend'));
+      input.addListener('start', 'all', () => console.log('start'));
+      input.addListener('stop', 'all', () => console.log('stop'));
+      input.addListener('reset', 'all', () => console.log('reset'));
+    });
+  }
+
   render() {
     return (
-      <></>
+      <>{this.props.children}</>
     );
   }
 }
-
-ReactDOM.render(<MidiContext />, document.getElementById('root'));
 
 export default MidiContext;
